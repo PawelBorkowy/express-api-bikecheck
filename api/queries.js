938e4d8f94954caa340bd743bef246892fbc34f2
BodzDev <borkowy.p@gmail.com>
2023-04-09 19:48:44 +0200

    update

--- api/queries.js
+++ api/queries.js
@@ -1,12 +1,13 @@
 const {Pool} = require('pg')
+require('dotenv').config()
 
 // Database config
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'api-bikecheck',
-    password: 'postgresSQL',
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 
 // Create Conway table 
